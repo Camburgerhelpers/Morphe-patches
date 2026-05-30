e35a3ff247ee5e7ce62459e3e3874674d7f5ded3
Quantro100 <quantro100@users.noreply.github.com>
2026-05-30 11:06:04 +0200

    Initial commit: AliExpress Morphe patches

---
+++ patches/build.gradle.kts
@@ -0,0 +1,36 @@
+group = "aliexpress"
+
+patches {
+    about {
+        name = "AliExpress Patches"
+        description = "Morphe patches for AliExpress"
+        source = "git@github.com:user/aliexpress-patches.git"
+        author = "user"
+        contact = "na"
+        website = "https://github.com/user/aliexpress-patches"
+        license = "GNU General Public License v3.0"
+    }
+}
+
+dependencies {
+    implementation(libs.gson)
+    implementation(libs.morphe.patches.library)
+}
+
+tasks {
+    register<JavaExec>("generatePatchesList") {
+        description = "Build patch with patch list"
+        dependsOn(build)
+        classpath = sourceSets["main"].runtimeClasspath
+        mainClass.set("app.morphe.util.PatchListGeneratorKt")
+    }
+    publish {
+        dependsOn("generatePatchesList")
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs = listOf("-Xcontext-parameters")
+    }
+}
---
+++ patches/src/main/kotlin/app/morphe/patches/aliexpress/utils/Constants.kt
@@ -0,0 +1,6 @@
+package app.morphe.patches.aliexpress.utils
+
+internal object Constants {
+    val COMPATIBILITY_ALIEXPRESS = arrayOf("com.alibaba.aliexpresshd")
+    const val PREF_DESCRIPTOR = "Lapp/morphe/extension/aliexpress/patches/PreferencesPatch;"
+}
